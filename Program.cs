ba9f70f1a040421ade56a7c3599d4a3243bb49e2
muflon <mfedor722@gmail.com>
2024-02-28 19:39:07 +0300

    Создание программы

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿string[] input = {"Машина", "Робот", "Кот", "р"};
+string[] resarray(string[] input){
+    int count = 0;
+    foreach (string i in input){
+        if (i.Length <=3){
+            count++;
+        }
+    }
+    string[] res = new string[count];
+    int index = 0;
+    foreach (string i in input){
+        if (i.Length <=3){
+            res[index]=i;
+            index++;
+        }
+    }
+    return res;
+}
+string[] res=resarray(input);
+Console.WriteLine($"[ {string.Join(", ", input)} ]");
+Console.WriteLine($"[ {string.Join(", ", res)} ]");
